b63122391eb010dd5d1419b16e0c47980ca3c3ad
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-03-14 15:21:53 +0100

    more quiet

--- .travis/setup_linux.bash
+++ .travis/setup_linux.bash
@@ -2,13 +2,13 @@
 
 cd "$TRAVIS_BUILD_DIR" || exit 1
 
-wget https://cdn.azul.com/zulu/bin/zulu13.29.9-ca-jdk13.0.2-linux_x64.tar.gz
-tar zxvf zulu13.29.9-ca-jdk13.0.2-linux_x64.tar.gz
+wget -q https://cdn.azul.com/zulu/bin/zulu13.29.9-ca-jdk13.0.2-linux_x64.tar.gz
+tar zxvf zulu13.29.9-ca-jdk13.0.2-linux_x64.tar.gz >/dev/null 2>&1t
 export JAVA_HOME=$TRAVIS_BUILD_DIR/zulu13.29.9-ca-jdk13.0.2-linux_x64
 echo "JAVA_HOME=$JAVA_HOME"
 export PATH=$JAVA_HOME/bin:$PATH
 echo "PATH=$PATH"
 echo "JAVA_VERSION=$(java -version)"
 echo "MVN_VERSION=$(mvn -version)"
-wget https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_linux-x64_bin.tar.gz
-tar zxvf openjdk-14_linux-x64_bin.tar.gz
+wget -q https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_linux-x64_bin.tar.gz
+tar zxvf openjdk-14_linux-x64_bin.tar.gz >/dev/null 2>&1t
