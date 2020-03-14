624a90cfc865d17aafbd97313b4d64a2bd90d70b
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-03-14 16:27:35 +0100

    make update manager readable

--- src/main/java/com/exxeta/correomqtt/plugin/update/PluginUpdateManager.java
+++ src/main/java/com/exxeta/correomqtt/plugin/update/PluginUpdateManager.java
@@ -33,62 +33,57 @@ public class PluginUpdateManager {
         String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8);
 
         if (new File(decodedPath + File.pathSeparator + "plugins.json").exists()) {
-
             List<UpdateRepository> repos = Collections.singletonList(new DefaultUpdateRepository("bundled", new File(decodedPath).toURI().toURL()));
-
             UpdateManager updateManager = new UpdateManager(pluginSystem, repos);
-
-            // >> keep system up-to-date <<
-            boolean systemUpToDate = true;
-
-            // check for updates
-            if (updateManager.hasUpdates()) {
-                List<PluginInfo> updates = updateManager.getUpdates();
-                LOGGER.info("Found {} plugin updates", updates.size());
-                for ( PluginInfo plugin : updates ) {
-                    LOGGER.info("Found update for plugin '{}'", plugin.id);
-                    PluginInfo.PluginRelease lastRelease = updateManager.getLastPluginRelease(plugin.id);
-                    String lastVersion = lastRelease.version;
-                    String installedVersion = pluginSystem.getPlugin(plugin.id).getDescriptor().getVersion();
-                    LOGGER.info("Update plugin '{}' from version {} to version {}", plugin.id, installedVersion, lastVersion);
-                    boolean updated = updateManager.updatePlugin(plugin.id, lastVersion);
-                    if (updated) {
-                        LOGGER.info("Updated plugin '{}'", plugin.id);
-                    } else {
-                        LOGGER.error("Cannot update plugin '{}'", plugin.id);
-                        systemUpToDate = false;
-                    }
-                }
-            } else {
-                LOGGER.info("No updates found");
-            }
-
-            // check for available (new) plugins
-            if (updateManager.hasAvailablePlugins()) {
-                List<PluginInfo> availablePlugins = updateManager.getAvailablePlugins();
-                LOGGER.info("Found {} available plugins", availablePlugins.size());
-                for ( PluginInfo plugin : availablePlugins ) {
-                    LOGGER.info("Found available plugin '{}'", plugin.id);
-                    PluginInfo.PluginRelease lastRelease = updateManager.getLastPluginRelease(plugin.id);
-                    String lastVersion = lastRelease.version;
-                    LOGGER.info("Install plugin '{}' with version {}", plugin.id, lastVersion);
-                    boolean installed = updateManager.installPlugin(plugin.id, lastVersion);
-                    if (installed) {
-                        LOGGER.info("Installed plugin '{}'", plugin.id);
-                    } else {
-                        LOGGER.error("Cannot install plugin '{}'", plugin.id);
-                        systemUpToDate = false;
-                    }
-                }
-            } else {
-                LOGGER.info("No available plugins found");
-            }
-
-            if (systemUpToDate) {
-                LOGGER.info("Plugins up-to-date");
-            }
+            updateExisitingPlugins(updateManager);
+            installNewPlugins(updateManager);
         }else{
             LOGGER.info("No default plugins available.");
+        }
+    }
+
+    private void installNewPlugins(UpdateManager updateManager) {
+        // check for available (new) plugins
+        if (updateManager.hasAvailablePlugins()) {
+            List<PluginInfo> availablePlugins = updateManager.getAvailablePlugins();
+            LOGGER.info("Found {} available plugins", availablePlugins.size());
+            for ( PluginInfo plugin : availablePlugins ) {
+                LOGGER.info("Found available plugin '{}'", plugin.id);
+                PluginInfo.PluginRelease lastRelease = updateManager.getLastPluginRelease(plugin.id);
+                String lastVersion = lastRelease.version;
+                LOGGER.info("Install plugin '{}' with version {}", plugin.id, lastVersion);
+                boolean installed = updateManager.installPlugin(plugin.id, lastVersion);
+                if (installed) {
+                    LOGGER.info("Installed plugin '{}'", plugin.id);
+                } else {
+                    LOGGER.error("Cannot install plugin '{}'", plugin.id);
+                }
+            }
+        } else {
+            LOGGER.info("No available plugins found");
+        }
+    }
+
+    private void updateExisitingPlugins(UpdateManager updateManager){
+        // check for updates
+        if (updateManager.hasUpdates()) {
+            List<PluginInfo> updates = updateManager.getUpdates();
+            LOGGER.info("Found {} plugin updates", updates.size());
+            for ( PluginInfo plugin : updates ) {
+                LOGGER.info("Found update for plugin '{}'", plugin.id);
+                PluginInfo.PluginRelease lastRelease = updateManager.getLastPluginRelease(plugin.id);
+                String lastVersion = lastRelease.version;
+                String installedVersion = pluginSystem.getPlugin(plugin.id).getDescriptor().getVersion();
+                LOGGER.info("Update plugin '{}' from version {} to version {}", plugin.id, installedVersion, lastVersion);
+                boolean updated = updateManager.updatePlugin(plugin.id, lastVersion);
+                if (updated) {
+                    LOGGER.info("Updated plugin '{}'", plugin.id);
+                } else {
+                    LOGGER.error("Cannot update plugin '{}'", plugin.id);
+                }
+            }
+        } else {
+            LOGGER.info("No updates found");
         }
     }
 }
