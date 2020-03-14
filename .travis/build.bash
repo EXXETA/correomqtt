9c57d51ae0a8728faf316de752fa2d00026c5373
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-03-14 15:54:25 +0100

    improved output

--- .travis/build.bash
+++ .travis/build.bash
@@ -2,6 +2,7 @@
 
 cd "$TRAVIS_BUILD_DIR" || exit 1
 
+echo "==== DECLARE CORREO VERSION ===="
 export CORREO_VERSION
 
 if [ -n "$TRAVIS_TAG" ]; then
@@ -16,37 +17,59 @@ echo "CORREO_VERSION is $CORREO_VERSION"
 echo "==== INSTALL DEPENDENCIES ===="
 
 if [ "$1" = "osx" ]; then
-  echo "Downloading Java 13"
+  echo -n "Downloading Java 13 ..."
   wget -q https://cdn.azul.com/zulu/bin/zulu13.29.9-ca-jdk13.0.2-macosx_x64.tar.gz
+  echo " done"
+  echo -n "Extracting Java 13 ..."
   tar zxvf zulu13.29.9-ca-jdk13.0.2-macosx_x64.tar.gz >/dev/null 2>&1
+  echo " done"
   export JAVA_HOME=$TRAVIS_BUILD_DIR/zulu13.29.9-ca-jdk13.0.2-macosx_x64
   export PATH=$JAVA_HOME/bin:$PATH
-  echo "Downloading Java 14"
+  echo -n "Downloading Java 14 ..."
   wget -q https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_osx-x64_bin.tar.gz
+  echo " done"
+  echo -n "Extracting Java 14 ..."
   tar zxvf openjdk-14_osx-x64_bin.tar.gz >/dev/null 2>&1
+  echo " done"
 elif [ "$1" = "linux" ]; then
-  echo "Downloading Java 13"
+  echo -n "Downloading Java 13 ..."
   wget -q https://cdn.azul.com/zulu/bin/zulu13.29.9-ca-jdk13.0.2-linux_x64.tar.gz
+  echo " done"
+  echo -n "Extracting Java 13 ..."
   tar zxvf zulu13.29.9-ca-jdk13.0.2-linux_x64.tar.gz >/dev/null 2>&1
+  echo " done"
   export JAVA_HOME=$TRAVIS_BUILD_DIR/zulu13.29.9-ca-jdk13.0.2-linux_x64
   export PATH=$JAVA_HOME/bin:$PATH
-  echo "Downloading Java 14"
+  echo -n "Downloading Java 14 ..."
   wget -q https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_linux-x64_bin.tar.gz
+  echo " done"
+  echo -n "Extracting Java 14 ..."
   tar zxvf openjdk-14_linux-x64_bin.tar.gz >/dev/null 2>&1
+  echo " done"
 elif [ "$1" = "windows" ]; then
   echo "Downloading Unzip"
   wget -q http://stahlworks.com/dev/unzip.exe
-  echo "Downloading Java 13"
+  echo " done"
+  echo -n "Downloading Java 13 ..."
   wget -q --no-check-certificate https://cdn.azul.com/zulu/bin/zulu13.29.9-ca-jdk13.0.2-win_x64.zip
+  echo " done"
+  echo -n "Extracting Java 13 ..."
   unzip -q zulu13.29.9-ca-jdk13.0.2-win_x64.zip
+  echo " done"
   mv zulu13.29.9-ca-jdk13.0.2-win_x64 zulu13
   echo "Downloading Maven"
   wget -q --no-check-certificate https://mirror.dkd.de/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
+  echo " done"
+  echo -n "Extracting Maven ..."
   unzip -q apache-maven-3.6.3-bin.zip
+  echo " done"
   mv apache-maven-3.6.3 maven
   echo "Downloading WIX"
   wget -q --no-check-certificate https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip
+  echo " done"
+  echo -n "Extracting WIX ..."
   unzip -q wix311-binaries.zip
+  echo " done"
   mv wix311-binaries wix
   ls -l
   export JAVA_HOME=$TRAVIS_BUILD_DIR/zulu13
@@ -55,19 +78,23 @@ elif [ "$1" = "windows" ]; then
   export MAVEN_HOME=$TRAVIS_BUILD_DIR/maven
   export PATH=$M2_HOME/bin:$PATH
   export PATH=$TRAVIS_BUILD_DIR/wix:$PATH
-  echo "Downloading Java 14"
+  echo -n "Downloading Java 14 ..."
   wget -q https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_windows-x64_bin.zip
+  echo " done"
+  echo -n "Extracting Java 14 ..."
   unzip -q openjdk-14_windows-x64_bin.zip
+  echo " done"
 fi
 
 echo "JAVA_HOME=$JAVA_HOME"
 echo "PATH=$PATH"
-echo "JAVA_VERSION="`java -version`
-echo "MVN_VERSION="`mvn -version`
+echo "JAVA_VERSION"
+java -version
+echo "MVN_VERSION"
+mvn -version
 
 PLUGIN_UPDATE_DATE=`date +"%Y-%m-%d"`
 PLUGIN_JSON_TEMPLATE="{\"id\": \"PLUGIN_ID\",\"releases\": [{\"PLUGIN_VERSION\": \"PLUGIN_VERSION\",\"date\": \"$PLUGIN_UPDATE_DATE\", \"url\": \"PLUGIN_JAR\"}]}"
-
 
 echo "==== SET CORREO VERSION ===="
 mvn versions:set -DnewVersion="$CORREO_VERSION"
@@ -77,13 +104,11 @@ echo "==== PREBUILD CORREO ===="
 mvn clean install -DskipTests=true
 
 function build_plugin() {
-
   local PLUGIN_VERSION=$1 && shift
   local REPO_NAME=$1 && shift
   local PLUGIN_ID=$1 && shift
 
   echo "==== BUILD PLUGIN: $PLUGIN_ID:$PLUGIN_VERSION ===="
-
   git clone https://github.com/EXXETA/"$REPO_NAME".git --branch "v$PLUGIN_VERSION" --single-branch
   cd "$REPO_NAME" || exit 1
   mvn versions:use-dep-version -DdepVersion="$CORREO_VERSION" -Dincludes=com.exxeta:correomqtt
@@ -115,13 +140,10 @@ echo "]" >> plugins.json
 mv plugins.json src/main/resources/com/exxeta/correomqtt/plugin/update/
 
 echo "==== BUILD CORREO ===="
-
 #build again with plugins
 mvn clean install -DskipTests=true
 
-
 echo "==== PACKAGE CORREO ===="
-
 if [ "$1" = "osx" ]; then
   ./jdk-14.jdk/Contents/Home/bin/jpackage \
     --type dmg \
@@ -164,6 +186,7 @@ elif [ "$1" = "windows" ]; then
     --vendor "EXXETA AG"
 
   #check if release and deploy manually to github because deploy in windows not working
+  echo "==== DEPLOY ===="
   echo "$TRAVIS_TAG"
   if [ -n "$TRAVIS_TAG" ]; then
     echo "tag set -> deploy release to github";
