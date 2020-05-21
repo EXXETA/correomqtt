2d5d6b355f429f5278951157354cad841766ca2a
Reinhold, Frieder <frieder.reinhold@exxeta.com>
2020-05-21 19:52:57 +0200

    fixes old config import, fixes logback file loading

--- src/main/java/org/correomqtt/gui/theme/IconMode.java
+++ src/main/java/org/correomqtt/gui/theme/IconMode.java
@@ -1,5 +1,10 @@
 package org.correomqtt.gui.theme;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum IconMode {
     BLACK("black"),WHITE("white");
 
@@ -12,5 +17,19 @@ public enum IconMode {
     @Override
     public String toString() {
         return iconMode;
+    }
+
+    @JsonValue
+    public String getIconMode() {
+        return iconMode;
+    }
+
+    @JsonCreator
+    public static IconMode forValue(String iconMode) {
+
+        return Arrays.stream(IconMode.values())
+                .filter(im -> im.iconMode.equalsIgnoreCase(iconMode))
+                .findFirst()
+                .orElse(BLACK);
     }
 }
