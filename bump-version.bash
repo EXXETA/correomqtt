f9af22e8d916ca60e22ab68012a9cab3eb5e18a6
oemel09 <github@oemel09.de>
2020-05-21 22:24:38 +0200

    Aborts script if there is no leading 'v'

--- bump-version.bash
+++ bump-version.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ $1 != v* ]] ; then
+  echo "Aborting: version must begin with a 'v'"
+  exit 1
+fi
 echo "set version to $1"
 mvn versions:set -DnewVersion="$1"
 echo -n "$1" > ./src/main/resources/org/correomqtt/business/utils/version.txt
