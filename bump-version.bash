53075d2b47a31dc7d66711136479ea899485baf5
oemel09 <github@oemel09.de>
2020-05-22 00:08:53 +0200

    Script adds the 'v' automatically in from of the version for the version.txt

--- bump-version.bash
+++ bump-version.bash
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $1 != v* ]] ; then
-  echo "Aborting: version must begin with a 'v'"
-  exit 1
-fi
 echo "set version to $1"
 mvn versions:set -DnewVersion="$1"
-echo -n "$1" > ./src/main/resources/org/correomqtt/business/utils/version.txt
+echo -n "v$1" > ./src/main/resources/org/correomqtt/business/utils/version.txt
 echo version.txt: $(cat ./src/main/resources/org/correomqtt/business/utils/version.txt)
