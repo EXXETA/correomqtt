14e99b43287e6700fd0b2e4a9ae399a198af3e41
Frieder Reinhold <reinhold@trigon-media.com>
2020-05-22 16:42:28 +0200

    Fixed logging

--- src/main/java/org/correomqtt/CorreoPreloader.java
+++ src/main/java/org/correomqtt/CorreoPreloader.java
@@ -1,6 +1,7 @@
 package org.correomqtt;
 
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.classic.util.ContextInitializer;
 import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.util.StatusPrinter;
@@ -16,6 +17,7 @@ import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
+import org.apache.commons.io.FileUtils;
 import org.correomqtt.business.dispatcher.PreloadingDispatcher;
 import org.correomqtt.business.dispatcher.PreloadingObserver;
 import org.correomqtt.business.services.SettingsService;
@@ -24,6 +26,7 @@ import org.correomqtt.gui.controller.PreloaderViewController;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class CorreoPreloader extends Preloader implements PreloadingObserver {
 
@@ -56,19 +59,16 @@ public class CorreoPreloader extends Preloader implements PreloadingObserver {
     private void setLoggerFilePath() {
         // Set the path for file logging to user directory.
         System.setProperty("correomqtt-logfile", SettingsService.getInstance().getLogPath());
-        System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "logger-config.xml");
 
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        ContextInitializer ci = new ContextInitializer(lc);
-        lc.reset();
-        try {
-            //I prefer autoConfig() over JoranConfigurator.doConfigure() so I wouldn't need to find the file myself.
-            ci.autoConfig();
-        } catch (JoranException e) {
-            // StatusPrinter will try to log this
-            e.printStackTrace(); //TODO
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.reset();
+        JoranConfigurator configurator = new JoranConfigurator();
+        try(InputStream configStream = CorreoPreloader.class.getResourceAsStream("logger-config.xml")) {
+            configurator.setContext(loggerContext);
+            configurator.doConfigure(configStream);
+        } catch (JoranException | IOException e) {
+            e.printStackTrace();
         }
-        StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
     }
 
     @Override
