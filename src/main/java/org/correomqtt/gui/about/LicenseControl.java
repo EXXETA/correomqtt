edc91fd532d079592631681c703ca4c8899ee236
Frieder <reinhold@trigon-media.com>
2024-01-04 17:58:27 +0100

    Moved licenses to a json file

---
+++ src/main/java/org/correomqtt/gui/about/LicenseControl.java
@@ -0,0 +1,34 @@
+package org.correomqtt.gui.about;
+
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import org.correomqtt.gui.utils.HostServicesHolder;
+
+public class LicenseControl extends HBox {
+
+    private final License license;
+
+    public LicenseControl(License license) {
+        super();
+        this.license = license;
+        Hyperlink repoLink = new Hyperlink();
+        repoLink.setText(license.name());
+        activateLink(repoLink, license.repoLink());
+        Label label = new Label();
+        label.setPrefHeight(23);
+        label.setText("licensed under");
+        Hyperlink licenseLink = new Hyperlink();
+        licenseLink.setText(license.license());
+        activateLink(licenseLink, license.licenseLink());
+        this.getChildren().addAll(repoLink, label, licenseLink);
+    }
+
+    private void activateLink(Hyperlink control, String link) {
+        control.setOnAction(t -> HostServicesHolder.getInstance()
+                .getHostServices()
+                .showDocument(link)
+        );
+    }
+}
+
