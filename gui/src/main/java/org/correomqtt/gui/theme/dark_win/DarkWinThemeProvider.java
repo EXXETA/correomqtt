b8c43f6aa3e72f86c692afebc5096f85213446af
Thomas <thomas.hagel@exxeta.com>
2025-04-09 09:28:59 +0200

    #131: add dark themes for mac and win

---
+++ gui/src/main/java/org/correomqtt/gui/theme/dark_win/DarkWinThemeProvider.java
@@ -0,0 +1,32 @@
+package org.correomqtt.gui.theme.dark_win;
+
+import javafx.scene.paint.Color;
+import org.correomqtt.gui.plugin.spi.ThemeProviderHook;
+import org.correomqtt.gui.theme.BaseThemeProvider;
+import org.correomqtt.gui.theme.IconMode;
+import org.correomqtt.gui.theme.ThemeProvider;
+import org.pf4j.Extension;
+
+@Extension
+public class DarkWinThemeProvider extends BaseThemeProvider implements ThemeProvider, ThemeProviderHook {
+
+    @Override
+    public String getName() {
+        return "Dark Windows";
+    }
+
+    @Override
+    public String getCss() {
+        return getCssFromInputStream(DarkWinThemeProvider.class.getResourceAsStream("dark_win.css"));
+    }
+
+    @Override
+    public IconMode getIconMode() {
+        return IconMode.WHITE;
+    }
+
+    @Override
+    public Color getBackgroundColor() {
+        return Color.web("#313131");
+    }
+}
