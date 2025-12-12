3a62ffbefe0411df677dd3895425ec9d60ed63e8
Thomas <thomas.hagel@exxeta.com>
2025-12-12 15:21:06 +0100

    Revert "#131: update"
    This reverts commit c9c0dfdcb235e39f9f0205ebb433b8f1c8ec6491.

--- gui/src/main/java/org/correomqtt/gui/theme/light/LightThemeProvider.java
+++ gui/src/main/java/org/correomqtt/gui/theme/light/LightThemeProvider.java
@@ -1,4 +1,4 @@
-package org.correomqtt.gui.theme.dark_breeze;
+package org.correomqtt.gui.theme.light;
 
 import javafx.scene.paint.Color;
 import org.correomqtt.gui.theme.BaseThemeProvider;
@@ -8,26 +8,25 @@ import org.correomqtt.gui.plugin.spi.ThemeProviderHook;
 import org.pf4j.Extension;
 
 @Extension
-public class DarkBreezeThemeProvider extends BaseThemeProvider implements ThemeProvider, ThemeProviderHook {
+public class LightThemeProvider extends BaseThemeProvider implements ThemeProvider, ThemeProviderHook {
 
     @Override
     public String getName() {
-        return "Dark Breeze";
+        return "Light";
     }
 
     @Override
     public String getCss() {
-        System.out.println("DTP");
-        return getCssFromInputStream(DarkBreezeThemeProvider.class.getResourceAsStream("dark_breeze.css"));
+        return getCssFromInputStream(LightThemeProvider.class.getResourceAsStream("light.css"));
     }
 
     @Override
     public IconMode getIconMode() {
-        return IconMode.WHITE;
+        return IconMode.BLACK;
     }
 
     @Override
     public Color getBackgroundColor() {
-        return Color.web("#313131");
+        return Color.web("#eff0f1");
     }
 }
